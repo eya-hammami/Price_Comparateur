f7e55532f57b0859ded48d894572775112dd9b64
mohamedamiine1 <mohammedaminehedhili@esprit.tn>
2025-05-12 19:26:55 +0100

    rem acc

--- KAA app/backend/models/user_model.py
+++
@@ -1,8 +0,0 @@
-def create_user(cursor, full_name, email, hashed_password):
-    sql = "INSERT INTO users (full_name, email, password) VALUES (%s, %s, %s)"
-    cursor.execute(sql, (full_name, email, hashed_password))
-
-def get_user_by_email(cursor, email):
-    sql = "SELECT * FROM users WHERE email = %s"
-    cursor.execute(sql, (email,))
-    return cursor.fetchone() 
